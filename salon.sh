9e1c0042f0c0795a7fd77f42aad1cebc91cabdf9
Karindash <karindashr@gmail.com>
2025-07-03 14:03:38 +0700

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -6,7 +6,6 @@ echo -e "\n~~~~~ MY SALON ~~~~~"
 echo -e "\nWelcome to My Salon, how can I help you?"
 
 MAIN_MENU() {
-  # Tampilkan daftar layanan
   SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
   echo "$SERVICES" | while IFS="|" read SERVICE_ID SERVICE_NAME
   do
@@ -15,18 +14,15 @@ MAIN_MENU() {
 
   read SERVICE_ID_SELECTED
 
-  # Cek apakah layanan valid
   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id=$SERVICE_ID_SELECTED")
   if [[ -z $SERVICE_NAME ]]
   then
     echo -e "\nI could not find that service. What would you like today?"
     MAIN_MENU
   else
-    # Minta nomor telepon
     echo -e "\nWhat's your phone number?"
     read CUSTOMER_PHONE
 
-    # Cek apakah pelanggan sudah terdaftar
     CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
     if [[ -z $CUSTOMER_NAME ]]
@@ -38,11 +34,9 @@ MAIN_MENU() {
 
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
-    # Minta waktu janji temu
     echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
     read SERVICE_TIME
 
-    # Masukkan ke tabel appointments
     INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
 
     echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
